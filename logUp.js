84c23702920c9082f8fe4c07287638e35a933524
Jaime Cantillo <cantillojaime10k@gmail.com>
2023-10-09 00:00:18 -0500

    Sprint - 002 (Modified)

--- logUp.js
+++ logUp.js
@@ -1,25 +1,20 @@
 document.addEventListener("DOMContentLoaded", function() {
-    var registroForm = document.getElementById("registroForm");
+  var registroForm = document.getElementById("registroForm");
   
-    registroForm.addEventListener("submit", function(event) {
-      event.preventDefault();
-  
+  registroForm.addEventListener("submit", function(event) {
       var nombre = document.querySelector('input[name="nombre"]').value;
-      
       var correo = document.querySelector('input[name="correo"]').value;
-      
       var contraseña = document.querySelector('input[name="contraseña"]').value;
-      
       var fechaNacimiento = document.querySelector('input[name="fecha-nacimiento"]').value;
-     
       var sexo = document.querySelector('select[name="sexo"]').value;
-  
-   
+      
       if (nombre === "" || correo === "" || contraseña === "" || fechaNacimiento === "" || sexo === "") {
-        alert("Por favor, completa todos los campos.");
+          event.preventDefault(); 
+          alert("Por favor, completa todos los campos.");
       } 
-      else if (contraseña.length < 8) {
-        alert("La contraseña debe tener al menos 8 caracteres.");
-     }
+      else if (contraseña.length !== 8) {
+          event.preventDefault();
+          alert("La contraseña debe tener exactamente 8 caracteres.");
+ }
 });
 });
